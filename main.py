7b62ca46d6c96f071d6f5af50e6ce225264c9e20
Joel Braun <joel.braun1999@gmail.com>
2023-02-06 20:50:42 +0200

    added functionality to analyze all entered data points

--- main.py
+++ main.py
@@ -63,57 +63,90 @@ print("Scoring: " + str(sk.metrics.mean_squared_error(val_y, y_test)))
 
 print(rfc.feature_importances_)
 
-rules = []
-
 # The following code is partly taken from the sklearn documentation and modified to our needs
+# https://scikit-learn.org/stable/auto_examples/tree/plot_unveil_tree_structure.html#sphx-glr-auto-examples-tree-plot-unveil-tree-structure-py
 
 node_indicator = rfc.estimators_[0].decision_path(X_test)
 leaf_id = rfc.apply(X_test)
 
-feature = rfc.estimators_[0].tree_.feature
+available_features = rfc.estimators_[0].tree_.feature
 threshold = rfc.estimators_[0].tree_.threshold
+children_left = rfc.estimators_[0].tree_.children_left
+children_right = rfc.estimators_[0].tree_.children_right
 
 # print(node_indicator)
 
-#Be aware, removing this may break the code
+# Be aware, removing this may break the code
 X_test = X_test.reset_index()
 
-sample_id = 0
-# obtain ids of the nodes `sample_id` goes through, i.e., row `sample_id`
-node_index = node_indicator.indices[
-             node_indicator.indptr[sample_id]: node_indicator.indptr[sample_id + 1]
-             ]
+list_of_rules = []
 
-print("Rules used to predict sample {id}:\n".format(id=sample_id))
-for node_id in node_index:
-    # continue to the next node if it is a leaf node
-    if leaf_id[sample_id] == node_id:
-        continue
+for sample_id in X_test.index:
+    current_rule_set = []
+    new_rule = {}
 
-    # print("DEBUG" + str(sample_id))
-    # print("DEBUG" + str(node_id))
-    # print("DEBUG" + str(feature))
-    # print(train_db.columns)
-    # print("DEBUG TYPE" + str(type(feature)))
-    # print("DEBUG SIZE" + str(X_test.shape))
+    # obtain ids of the nodes `sample_id` goes through, i.e., row `sample_id`
+    node_index = node_indicator.indices[
+                 node_indicator.indptr[sample_id]: node_indicator.indptr[sample_id + 1]
+                 ]
 
-    # check if value of the split feature for sample 0 is below threshold
-    if X_test.loc[sample_id][X_test.columns[feature[node_id]]] <= threshold[node_id]:
-        threshold_sign = "<="
-    else:
-        threshold_sign = ">"
+    print("Rules used to predict sample {id}:\n".format(id=sample_id))
+    for node_id in node_index:
+        # continue to the next node if it is a leaf node
+        if leaf_id[sample_id] == node_id:
+            continue
 
-    print(
-        "decision node {node} : (X_test[{sample}, {feature}] = {value}) "
-        "{inequality} {threshold})".format(
-            node=node_id,
-            sample=sample_id,
-            feature=train_db.columns[feature[node_id]],
-            value= X_test.loc[sample_id][train_db.columns[feature[node_id]]],
-            inequality=threshold_sign,
-            threshold=threshold[node_id],
+        # print("DEBUG" + str(sample_id))
+        # print("DEBUG" + str(node_id))
+        # print("DEBUG" + str(feature))
+        # print(train_db.columns)
+        # print("DEBUG TYPE" + str(type(feature)))
+        # print("DEBUG SIZE" + str(X_test.shape))
+
+        greater = None
+        threshold_value = threshold[node_id]
+        feature = X_test.columns[available_features[node_id]]
+
+        # check if value of the split feature for sample 0 is below threshold
+        if X_test.loc[sample_id][X_test.columns[available_features[node_id]]] <= threshold[node_id]:
+            threshold_sign = "<="
+            greater = False
+        else:
+            threshold_sign = ">"
+            greater = True
+
+        print(
+            "decision node {node} : (X_test[{sample}, {feature}] = {value}) "
+            "{inequality} {threshold})".format(
+                node=node_id,
+                sample=sample_id,
+                feature=train_db.columns[available_features[node_id]],
+                value=X_test.loc[sample_id][train_db.columns[available_features[node_id]]],
+                inequality=threshold_sign,
+                threshold=threshold_value,
+            )
         )
-    )
+
+        new_rule = {
+            "greater": greater,
+            "threshold": threshold_value,
+            "feature": feature
+        }
+
+        current_rule_set.append(new_rule)
+
+        print("ID " + str(node_id))
+        print("last " + str(node_index[-1]))
+
+        if node_id == node_index[-2]:
+            if current_rule_set not in list_of_rules:
+                list_of_rules.append(current_rule_set)
+            print("Clearing ruleset")
+            current_rule_set = {}
+
+print(list_of_rules)
+
+#TODO Save to scv for further use in the next script to create plots
 
 # for data_point in y_test.rows:
 # This may need to be expanded to multiple trees in the future
@@ -126,9 +159,9 @@ for node_id in node_index:
 #    print(tree_struct.children_right[0])
 #    print(type(tree))
 
-fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(4, 4), dpi=800)
-sk.tree.plot_tree(rfc.estimators_[0],
-                  node_ids=True,
-                  feature_names=train_db.columns,
-                  filled=True)
-fig.savefig("rf_individualtree_debug" + str(1337) + ".png")
+#fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(4, 4), dpi=800)
+#sk.tree.plot_tree(rfc.estimators_[0],
+#                  node_ids=True,
+#                  feature_names=train_db.columns,
+#                  filled=True)
+#fig.savefig("rf_individualtree_debug" + str(1337) + ".png")
