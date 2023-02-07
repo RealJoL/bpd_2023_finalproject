d96f4454882066638a99a84074ed22848e10c339
Joel Braun <joel.braun1999@gmail.com>
2023-02-07 20:46:06 +0200

    Added reverse encoding of various data points, added rule set database as result. Additionally, two sample result files were added to the commit.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+from datetime import datetime
 import sklearn as sk
 import pandas as pd
-from matplotlib import pyplot as plt
 from pandas.core.dtypes.common import is_numeric_dtype
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
@@ -28,13 +29,27 @@ all_bad_db = all_bad_db.drop(columns=columns_to_drop)
 
 pre_proc_db = pd.concat([all_good_db, all_bad_db], ignore_index=True, verify_integrity=True)
 
+# Fill Sport and Dport for null to 0 and convert to int
+pre_proc_db[['Sport', 'Dport']] = pre_proc_db[['Sport', 'Dport']].fillna(value="0")
+pre_proc_db['Sport'] = pre_proc_db['Sport'].apply(lambda x: int(int(x, 16)))
+pre_proc_db['Dport'] = pre_proc_db['Dport'].apply(lambda x: int(int(x, 16)))
+
+pre_proc_db[['State']] = pre_proc_db[['State']].fillna(value="NULL")
+
 # Encode labels to numeric
 label_encs = {}
-
+le_encodings = {}
 for col in pre_proc_db:
     le = LabelEncoder()
-    le.fit(pre_proc_db[col].unique())
-    label_encs[col] = le
+    if not is_numeric_dtype(pre_proc_db[col]):
+        le.fit(pre_proc_db[col].unique())
+        le_dict = {}
+        for item in pre_proc_db[col].unique():
+            print(item)
+            transformed_item = le.transform([item])
+            le_dict[int(transformed_item[0])] = item
+        le_encodings[col] = le_dict
+        label_encs[col] = le
 
 train_db = pd.DataFrame(pre_proc_db)
 for col in pre_proc_db:
@@ -63,7 +78,7 @@ print("Scoring: " + str(sk.metrics.mean_squared_error(val_y, y_test)))
 
 print(rfc.feature_importances_)
 
-# The following code is partly taken from the sklearn documentation and modified to our needs
+# The following code is partly taken from the sklearn documentation and modified to fit our needs
 # https://scikit-learn.org/stable/auto_examples/tree/plot_unveil_tree_structure.html#sphx-glr-auto-examples-tree-plot-unveil-tree-structure-py
 
 node_indicator = rfc.estimators_[0].decision_path(X_test)
@@ -90,18 +105,11 @@ for sample_id in X_test.index:
                  node_indicator.indptr[sample_id]: node_indicator.indptr[sample_id + 1]
                  ]
 
-    print("Rules used to predict sample {id}:\n".format(id=sample_id))
+    # print("\nRules used to predict sample {id}:".format(id=sample_id))
     for node_id in node_index:
         # continue to the next node if it is a leaf node
         if leaf_id[sample_id] == node_id:
             continue
-
-        # print("DEBUG" + str(sample_id))
-        # print("DEBUG" + str(node_id))
-        # print("DEBUG" + str(feature))
-        # print(train_db.columns)
-        # print("DEBUG TYPE" + str(type(feature)))
-        # print("DEBUG SIZE" + str(X_test.shape))
 
         greater = None
         threshold_value = threshold[node_id]
@@ -115,17 +123,17 @@ for sample_id in X_test.index:
             threshold_sign = ">"
             greater = True
 
-        print(
-            "decision node {node} : (X_test[{sample}, {feature}] = {value}) "
-            "{inequality} {threshold})".format(
-                node=node_id,
-                sample=sample_id,
-                feature=train_db.columns[available_features[node_id]],
-                value=X_test.loc[sample_id][train_db.columns[available_features[node_id]]],
-                inequality=threshold_sign,
-                threshold=threshold_value,
-            )
-        )
+        # print(
+        #    "decision node {node} : (X_test[{sample}, {feature}] = {value}) "
+        #    "{inequality} {threshold})".format(
+        #        node=node_id,
+        #        sample=sample_id,
+        #        feature=train_db.columns[available_features[node_id]],
+        #        value=X_test.loc[sample_id][train_db.columns[available_features[node_id]]],
+        #        inequality=threshold_sign,
+        #        threshold=threshold_value,
+        #    )
+        # )
 
         new_rule = {
             "greater": greater,
@@ -135,18 +143,51 @@ for sample_id in X_test.index:
 
         current_rule_set.append(new_rule)
 
-        print("ID " + str(node_id))
-        print("last " + str(node_index[-1]))
-
         if node_id == node_index[-2]:
             if current_rule_set not in list_of_rules:
                 list_of_rules.append(current_rule_set)
-            print("Clearing ruleset")
             current_rule_set = {}
 
-print(list_of_rules)
+# print(list_of_rules)
+for ruleset in list_of_rules:
+    print(ruleset)
 
-#TODO Save to scv for further use in the next script to create plots
+rule_df = pd.DataFrame()
+
+for i, ruleset in enumerate(list_of_rules):
+    for rule in ruleset:
+        feature = rule['feature']
+        greater = rule['greater']
+        threshold = rule['threshold']
+        rule_df.at[i, str(feature) + '_greater'] = greater
+        rule_df.at[i, str(feature) + '_threshold'] = threshold
+        if feature in le_encodings.keys():
+            rule_df.at[i, str(feature) + '_greater_nonenc'] = greater
+            print("DEBUG val " + str(feature))
+            print("DEBUG " + str(threshold))
+            found_thr = None
+            if threshold in le_encodings[feature].keys():
+                print("yes")
+                found_thr = le_encodings[feature][threshold]
+
+            rule_df.at[i, str(feature) + '_threshold_nonenc'] = found_thr
+
+file_time = str("d"
+                + str(datetime.now().day)
+                + "-"
+                + str(datetime.now().hour)
+                + str(datetime.now().minute)
+                + str(datetime.now().second))
+
+rule_df.to_csv("./output/ruleset_"
+               + file_time
+               + ".csv")
+
+print(rule_df)
+print(le_encodings)
+
+with open('./output/encoding_' + file_time + '.json', 'w+') as f:
+    json.dump(le_encodings, f, indent=4)
 
 # for data_point in y_test.rows:
 # This may need to be expanded to multiple trees in the future
@@ -159,9 +200,9 @@ print(list_of_rules)
 #    print(tree_struct.children_right[0])
 #    print(type(tree))
 
-#fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(4, 4), dpi=800)
-#sk.tree.plot_tree(rfc.estimators_[0],
+# fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(4, 4), dpi=800)
+# sk.tree.plot_tree(rfc.estimators_[0],
 #                  node_ids=True,
 #                  feature_names=train_db.columns,
 #                  filled=True)
-#fig.savefig("rf_individualtree_debug" + str(1337) + ".png")
+# fig.savefig("rf_individualtree_debug" + str(1337) + ".png")
